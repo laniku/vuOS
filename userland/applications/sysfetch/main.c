95b8ca8524d94a2901789878b37baea84b710c35
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2021-03-22 10:34:25 -0400

    SYSFETCH: create main.c

---
+++ userland/applications/sysfetch/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("              ____  _____ \n");
+    printf(" _   ____  __/ __ |/ ___/ \n");
+    printf("| | / / / / / / / /|__ |  \n");
+    printf("| |/ / /_/ / /_/ /___/ /  \n");
+    printf("|___/|__,_/|____//____/   \n");
+    printf("__________________________\n");
+    printf("Kernel name: SEATRAY \n");
+    printf("CPU mode: Protected \n");
+    printf("Shell: vuSH \n");
+
+    return 0;
+}
