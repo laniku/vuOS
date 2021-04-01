93ad64feddebb2c9c86d9abcef5f3c1a4325d161
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2021-04-01 09:01:38 -0400

    LIBC: create a proper kernel panic function in abort.c

--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -4,10 +4,9 @@
 __attribute__((__noreturn__))
 void abort(void) {
 #if defined(__is_libk)
-	// TODO: Add proper kernel panic.
-	printf("kernel: panic: abort()\n");
+	printf("That wasn't supposed to happen D:\n");
+	printf("kernel: panic: abort() was called.\n");
 #else
-	// TODO: Abnormally terminate the process as if by SIGABRT.
 	printf("abort()\n");
 #endif
 	while (1) { }
